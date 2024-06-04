b602f34354920aa347b9ba0b05d7bef868f96f6a
King-James-II <jocundjames@gmail.com>
2024-06-04 04:53:54 -0400

    additional linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -135,7 +135,7 @@ def add_review(request):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200})
+            return JsonResponse({"status": 200, "posted": response})
         except Exception:
             return JsonResponse({"status": 401,
                                 "message": "Error in posting review"})
